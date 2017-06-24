50b596ffb45f6cdf7fc5ac8b53dc91e8e5cbb3fa
ra-bedo11 <abdoelmasryabdoelmasry@gmai.com>
2017-06-24 05:59:28 +0300

    commit

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -12,17 +12,27 @@ namespace ConsoleApplication3
         {
             ///https://github.com/ra-bedo11/C-Codes_1.git
             ///
-            float mf = float.MinValue;
-            float maxf = float.MaxValue;
 
-            Console.WriteLine("float min val =  {0}, float max val = {1}", mf, maxf);
-            Console.WriteLine("double min val = " + double.MinValue + " " + " double max val" + double.MaxValue);
-            Console.WriteLine("decimal min val = " + decimal.MinValue + " " + " decimal max val" + decimal.MaxValue);
+            //float mf = float.MinValue;
+            //float maxf = float.MaxValue;
 
-            float fvar = 1f / 3;
-            double dvar = 1d / 3;
-            decimal mvar = 1m / 3;
-            Console.Write(" float = {0},\n double = {1},\n decimal = {2} \n", fvar, dvar, mvar);
+            //Console.WriteLine("float min val =  {0}, float max val = {1}", mf, maxf);
+            //Console.WriteLine("double min val = " + double.MinValue + " " + " double max val" + double.MaxValue);
+            //Console.WriteLine("decimal min val = " + decimal.MinValue + " " + " decimal max val" + decimal.MaxValue);
+
+            //float fvar = 1f / 3;
+            //double dvar = 1d / 3;
+            //decimal mvar = 1m / 3;
+            // Console.Write(" float = {0},\n double = {1},\n decimal = {2} \n", fvar, dvar, mvar);
+
+            //char x = 'G';
+            //int xx = x;
+            //Console.WriteLine(xx);
+            //Console.WriteLine("char min val = " + Convert.ToInt16(char.MinValue) + " " + " char max val " + Convert.ToInt32(char.MaxValue));
+            //bool m = true;
+            //Console.WriteLine(m);
+            int c = Convert.ToInt16('a');
+            Console.WriteLine(c);
         }
     }
 }
